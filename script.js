be039e46a762a08926140b7ca81e2faf96d76328
arashjf <ario1313j@gmail.com>
2025-11-12 22:58:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("login-form");
+const toastContainer = document.getElementById("toastContainer");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const fname = document.getElementById("fname");
+  const pass = document.getElementById("pass");
+
+  if (fname.value === "Arash" && pass.value === "1234567") {
+    showtoast("✅ ورود با موفقیت انجام شد", "success");
+  } else {
+    showtoast("❌ نام کاربری یا رمز اشتباه است", "error");
+  }
+
+});
+
+function showtoast(message, type = "succes") {
+  const toast = document.createElement("div");
+  toast.classList.add("toast", type);
+  toast.textContent = message;
+  document.getElementById("toast-container").appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 2000);
+
+}
+
+document.getElementById("icon-pass").addEventListener("click", function () {
+  const passvalue = document.getElementById("pass");
+
+  const type =
+    passvalue.getAttribute("type") === "password" ? "text" : "password";
+
+  passvalue.setAttribute("type", type);
+
+  this.textContent = type === "password" ? "👁️" : "👁️‍🗨️";
+});
